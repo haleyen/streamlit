2e826bc44f8ec6f2d0fb2a7f90306132839e5451
Ha Le Yen <105425726+haleyen@users.noreply.github.com>
2024-04-05 11:43:15 +0700

    update app

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import streamlit as st
 import pandas as pd
 
-rfm_file = "result_rfm_quartile.csv"
+rfm_file = "result_rfm_quintile.csv"
 customer_transaction = 'data_customer_transaction.csv'
 RFM_segmentation_description = """
-            - **Champions**: Bought recently, buy often and spend the most! Reward them. Can be early adopters for new products. Will promote your brand.
-            - **Loyal Customers**: Spend good money with us often. Responsive to promotions. Upsell higher value products. Ask for reviews. Engage them.
-            - **Potential Loyalist**: Recent customers, but spent a good amount and bought more than once. Offer membership / loyalty program, recommend other products.
-            - **Recent Customers**: Bought most recently, but not often. Provide on-boarding support, give them early success, start building relationship.
-            - **Promising**: Recent shoppers, but haven’t spent much. Create brand awareness, offer free trials
-            - **Customers Needing Attention**: Above average recency, frequency and monetary values. May not have bought very recently though. Make limited time offers, Recommend based on past purchases. Reactivate them.
-            - **About To Sleep**: Below average recency, frequency and monetary values. Will lose them if not reactivated. Share valuable resources, recommend popular products / renewals at discount, reconnect with them.
-            - **At Risk**: Spent big money and purchased often. But long time ago. Need to bring them back! Send personalized emails to reconnect, offer renewals, provide helpful resources.
-            - **Can’t Lose Them**: Made biggest purchases, and often. But haven’t returned for a long time. Win them back via renewals or newer products, don’t lose them to competition, talk to them.
-            - **Hibernating**: Last purchase was long back, low spenders and low number of orders. Offer other relevant products and special discounts. Recreate brand value.
-            - **Lost**: Lowest recency, frequency and monetary scores. Revive interest with reach out campaign, ignore otherwise.           
+            - **Champions**: Bought recently, buy often and spend the most!
+            - **Loyal Customers**: Spend good money with us often. Responsive to promotions. 
+            - **Potential Loyalist**: Recent customers, but spent a good amount and bought more than once. 
+            - **Recent Customers**: Bought most recently, but not often. 
+            - **Promising**: Recent shoppers, but haven’t spent much. 
+            - **Customers Needing Attention**: Above average recency, frequency and monetary values. May not have bought very recently though.
+            - **About To Sleep**: Below average recency, frequency and monetary values. Will lose them if not reactivated. 
+            - **At Risk**: Spent big money and purchased often. But long time ago. 
+            - **Can’t Lose Them**: Made biggest purchases, and often. But haven’t returned for a long time.
+            - **Hibernating**: Last purchase was long back, low spenders and low number of orders. 
+            - **Lost**: Lowest recency, frequency and monetary scores.       
             """
 def FMscore(x,p,d):
     if x <= d[p][0.2]:
